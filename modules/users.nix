159cb8fcd5c70e0e5b8fac305b3e55320bbca031
Stefan Junker <mail@stefanjunker.de>
2023-01-25 10:50:02 +0100

    fix(modules/users): use keys_ prefix instead of key_

--- modules/users.nix
+++ modules/users.nix
@@ -1,6 +1,5 @@
 { inputs, lib, ... }: {
-  options.sshKeysAll = lib.mkOption {type = lib.types.listOf lib.types.path;};
-  config.sshKeysAll =
-    lib.attrValues
-    (lib.filterAttrs (name: _: lib.hasPrefix "key_" name) inputs);
+  options.sshKeysAll = lib.mkOption { type = lib.types.listOf lib.types.path; };
+  config.sshKeysAll = lib.attrValues
+    (lib.filterAttrs (name: _: lib.hasPrefix "keys_" name) inputs);
 }
