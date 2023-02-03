3ee73d0bec1e6bbb3837a38982262135216658f4
DavHau <hsngrmpf+github@gmail.com>
2023-02-03 21:49:09 +0700

    Add alejandra formatter

---
+++ modules/formatter.nix
@@ -0,0 +1,16 @@
+{
+  self,
+  lib,
+  ...
+}: {
+  perSystem = {
+    config,
+    self',
+    inputs',
+    pkgs,
+    ...
+  }: {
+    # define formatter used by `nix fmt`
+    formatter = pkgs.alejandra;
+  };
+}
