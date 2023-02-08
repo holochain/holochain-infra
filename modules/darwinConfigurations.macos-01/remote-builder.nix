8ec2ac0a9995912f8cf97c6fda16f0a06aaeb120
Stefan Junker <mail@stefanjunker.de>
2023-02-08 10:51:35 +0100

    feat(macos/remote-builder): declare authorized_keys

--- modules/darwinConfigurations.macos-01/remote-builder.nix
+++ modules/darwinConfigurations.macos-01/remote-builder.nix
@@ -20,5 +20,9 @@
   };
   system.activationScripts.postActivation.text = ''
     echo 'PATH=/nix/var/nix/profiles/default/bin:$PATH' > /Users/builder/.bashrc
+    mkdir -p /Users/builder/.ssh/
+    chmod 700 /Users/builder/.ssh/
+    chown builder:staff /Users/builder/.ssh/
+    echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ1K1ZYBnf3UqQbln5Z8DLYsXyJo6pRAFISPQ7lJZpoO root@linux-builder-01" > /Users/builder/.ssh/authorized_keys
   '';
 }
