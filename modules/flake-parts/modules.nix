ae0e079884e1b3f97f30689ce5dfabebd39230be
DavHau <hsngrmpf+github@gmail.com>
2023-02-09 15:35:46 +0700

    chore: move darwin modules to ./modules/darwin

--- modules/flake-parts/modules.nix
+++ modules/flake-parts/modules.nix
@@ -1,5 +1,24 @@
-{lib, ...}: {
+{lib, config, ...}: {
   options.flake.modules = lib.mkOption {
     type = lib.types.anything;
   };
+
+  config.flake.modules.darwin =
+    lib.mapAttrs
+    (_: m: "${../darwin}/${m}")
+    (builtins.readDir ../darwin);
+
+  config.flake.modules.nixos =
+    lib.mapAttrs
+    (_: m: "${../nixos}/${m}")
+    (builtins.readDir ../nixos);
+
+  config.flake.modules.flake-parts =
+    lib.mapAttrs
+    (_: m: "${../flake-parts}/${m}")
+    (builtins.readDir ../flake-parts);
+
+  # comapt to legacy schema
+  config.flake.nixosModules = config.flake.modules.nixos;
+  config.flake.darwinModules = config.flake.modules.darwin;
 }
