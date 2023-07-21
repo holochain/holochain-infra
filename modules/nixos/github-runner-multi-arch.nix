7d2b7c597e2c91f43bac051482a1dcb420b2a5ef
Stefan Junker <mail@stefanjunker.de>
2023-07-21 21:26:51 +0200

    feat: use github-runner from nixos-unstable
    this also adds instructions on how to update just the packages for the
    github-runner.

--- modules/nixos/github-runner-multi-arch.nix
+++ modules/nixos/github-runner-multi-arch.nix
@@ -1,13 +1,19 @@
 {
+  self,
   config,
   lib,
   magicPaths,
+  pkgs,
   ...
 }: let
   githubRunnersCfg = {
     count = 16;
     namePrefix = "multi-arch";
   };
+
+  nixpkgsGithubActionRunners' = pkgs.callPackage self.inputs.nixpkgsGithubActionRunners {};
+
+  package = nixpkgsGithubActionRunners'.github-runner;
 
   mkList = prefix: (builtins.genList
     (x: "${prefix}${githubRunnersCfg.namePrefix}-${builtins.toString x}")
@@ -19,6 +25,7 @@ in {
     (_: {
       replace = true;
       ephemeral = true;
+      inherit package;
       extraLabels = [githubRunnersCfg.namePrefix];
       tokenFile = magicPaths.githubRunnerHraTokenHostPath;
       url = "https://github.com/holochain/holochain";
@@ -27,6 +34,6 @@ in {
 
   # TODO where is NodeJS coming from and how can it be updated?
   nixpkgs.config.permittedInsecurePackages = [
-    "nodejs-16.20.0"
+    "nodejs-16.20.1"
   ];
 }
