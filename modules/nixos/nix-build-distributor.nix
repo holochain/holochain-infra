7a9098a78c517491c028a5d8bacab25f881fa657
Stefan Junker <mail@stefanjunker.de>
2023-10-02 10:43:56 +0200

    feat(nix-build-distributor): use all available machines
    make sure we run x86_64-linux only on machines that have disabled system
    integrity protection.

--- modules/nixos/nix-build-distributor.nix
+++ modules/nixos/nix-build-distributor.nix
@@ -7,17 +7,19 @@
 
   nix.distributedBuilds = true;
   nix.buildMachines = [
-    # macos-01 - system integrity protection enabled
-    # currently broken and needs to be reinstalled
+    # macos-01
+    # - m1 cpu
+    # - system integrity protection disabled
     {
       hostName = "167.235.13.208";
       sshUser = "builder";
       protocol = "ssh-ng";
-      system = "aarch64-darwin";
-      maxJobs = 0;
+      system = "x86_64-darwin";
+      maxJobs = 4;
       supportedFeatures = config.nix.settings.experimental-features;
     }
     # macos-02
+    # - intel CPU
     {
       hostName = "hydra-minion-2.holo.host";
       sshUser = "builder";
@@ -27,7 +29,9 @@
       supportedFeatures = config.nix.settings.experimental-features;
     }
 
-    # macos-03 - system integrity protection enabled
+    # macos-03
+    # - m1 cpu
+    # - system integrity protection enabled
     {
       hostName = "142.132.140.224";
       sshUser = "builder";
@@ -37,15 +41,9 @@
       supportedFeatures = config.nix.settings.experimental-features;
     }
 
-    # macos-04 - system integrity protection disabled
-    {
-      hostName = "167.235.38.111";
-      sshUser = "builder";
-      protocol = "ssh-ng";
-      system = "x86_64-darwin";
-      maxJobs = 0;
-      supportedFeatures = config.nix.settings.experimental-features;
-    }
+    # macos-04
+    # - m1 cpu
+    # - system integrity protection enabled
     {
       hostName = "167.235.38.111";
       sshUser = "builder";
