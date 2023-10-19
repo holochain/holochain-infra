6449a30b13cfc1cad79a2401d687b60bb5b2d305
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-10-19 15:22:15 +0100

    Disable macos-02 and bump runner versions

--- modules/nixos/nix-build-distributor.nix
+++ modules/nixos/nix-build-distributor.nix
@@ -17,14 +17,14 @@
       supportedFeatures = config.nix.settings.experimental-features;
     }
     # macos-02
-    {
-      hostName = "hydra-minion-2.holo.host";
-      sshUser = "builder";
-      protocol = "ssh-ng";
-      system = "x86_64-darwin";
-      maxJobs = 4;
-      supportedFeatures = config.nix.settings.experimental-features;
-    }
+    # {
+    #   hostName = "hydra-minion-2.holo.host";
+    #   sshUser = "builder";
+    #   protocol = "ssh-ng";
+    #   system = "x86_64-darwin";
+    #   maxJobs = 4;
+    #   supportedFeatures = config.nix.settings.experimental-features;
+    # }
 
     # macos-03
     {
