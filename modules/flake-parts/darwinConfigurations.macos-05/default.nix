61d05af99709b73a842ffa31a0ffa6891eebb69b
Stefan Junker <mail@stefanjunker.de>
2023-11-07 15:07:04 +0100

    feat: init macos-05 darwin config

---
+++ modules/flake-parts/darwinConfigurations.macos-05/default.nix
@@ -0,0 +1,19 @@
+{
+  self,
+  lib,
+  inputs,
+  config,
+  ...
+}: let
+  system = "aarch64-darwin";
+in {
+  flake.darwinConfigurations.macos-05 = inputs.darwin.lib.darwinSystem {
+    inherit system inputs;
+    modules = [
+      ./configuration.nix
+      ../../nixos/macos.nix
+      ../../nixos/macos-remote-builder.nix
+      inputs.home-manager.darwinModules.home-manager
+    ];
+  };
+}
