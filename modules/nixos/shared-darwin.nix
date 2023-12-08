97d43b3ea11e6f84ab17da9a34365f85f80c5077
ThetaSinner <greatdjonfire@hotmail.co.uk>
2023-12-08 14:21:44 +0000

    Fix reference to nix-store

--- modules/nixos/shared-darwin.nix
+++ modules/nixos/shared-darwin.nix
@@ -1,7 +1,7 @@
 { config, lib, ...}: {
   launchd.daemons.nix-gc.command = lib.mkForce ''
     echo Removing 0-size derivations if any exist...
-    ${config.nix.package}/bin/nix-store --query --referrers-closure $(find /nix/store -maxdepth 1 -type f -name '*.drv' -size 0)  | xargs nix-store --delete --ignore-liveness
+    ${config.nix.package}/bin/nix-store --query --referrers-closure $(find /nix/store -maxdepth 1 -type f -name '*.drv' -size 0)  | xargs ${config.nix.package}/bin/nix-store --delete --ignore-liveness
 
     ${config.nix.package}/bin/nix-collect-garbage ${config.nix.gc.options}
   '';
--- modules/nixos/shared-linux.nix
+++ modules/nixos/shared-linux.nix
@@ -1,6 +1,6 @@
 { config, ... }: {
   systemd.services.nix-gc.preStart = ''
     echo Removing 0-size derivations if any exist...
-    ${config.nix.package}/bin/nix-store --query --referrers-closure $(find /nix/store -maxdepth 1 -type f -name '*.drv' -size 0)  | xargs nix-store --delete --ignore-liveness
+    ${config.nix.package}/bin/nix-store --query --referrers-closure $(find /nix/store -maxdepth 1 -type f -name '*.drv' -size 0)  | xargs ${config.nix.package}/bin/nix-store --delete --ignore-liveness
   '';
 }
