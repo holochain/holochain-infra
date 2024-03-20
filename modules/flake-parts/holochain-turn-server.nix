bdb43291479a40bff73f1a6128d603ab0b8b9318
Stefan Junker <mail@stefanjunker.de>
2024-03-20 20:36:59 +0000

    holochain-turn-server: remove perSystem attr

--- modules/flake-parts/holochain-turn-server.nix
+++ modules/flake-parts/holochain-turn-server.nix
@@ -4,14 +4,6 @@
   lib,
   ...
 }: {
-  perSystem = {
-    # Arguments specific to the `perSystem` context.
-    ...
-  }: {
-    # system specific outputs like, apps, checks, packages
-
-    # packages = ...
-  };
   flake = {
     # system independent outputs like nixosModules, nixosConfigurations, etc.
 
