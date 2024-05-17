c12df90f097c2db13480dfc0d1f85d1dd799c7a8
Stefan Junker <mail@stefanjunker.de>
2024-05-17 10:57:26 +0200

    fixup! feat: introduce nixpkgsNix, set nix version to 2.20

--- modules/nixos/shared.nix
+++ modules/nixos/shared.nix
@@ -1,5 +1,5 @@
 {
-  self,
+  inputs,
   config,
   pkgs,
   lib,
@@ -17,7 +17,7 @@
     # ]
     ;
 
-  nix.package = lib.mkDefault self.inputs.nixpkgsNix.legacyPackages.${pkgs.stdenv.system}.nixVersions.nix_2_20;
+  nix.package = lib.mkDefault inputs.nixpkgsNix.legacyPackages.${pkgs.stdenv.system}.nixVersions.nix_2_20;
 
   nix.settings.extra-platforms =
     lib.mkIf pkgs.stdenv.isDarwin ["x86_64-darwin" "aarch64-darwin"];
