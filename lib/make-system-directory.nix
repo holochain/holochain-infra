2de81cf5afea6151157ef0a360c859997bbef73d
Stefan Junker <mail@stefanjunker.de>
2024-05-24 09:16:23 +0000

    chore: nix fmt

--- lib/make-system-directory.nix
+++ lib/make-system-directory.nix
@@ -1,47 +1,41 @@
-{ stdenv
-, closureInfo
-, pixz
-
-, # The files and directories to be placed in the directory.
+{
+  stdenv,
+  closureInfo,
+  pixz,
+  # The files and directories to be placed in the directory.
   # This is a list of attribute sets {source, target} where `source'
   # is the file system object (regular file or directory) to be
   # grafted in the file system at path `target'.
-  contents
-
-, # In addition to `contents', the closure of the store paths listed
+  contents,
+  # In addition to `contents', the closure of the store paths listed
   # in `packages' are also placed in the Nix store of the tarball.  This is
   # a list of attribute sets {object, symlink} where `object' if a
   # store path whose closure will be copied, and `symlink' is a
   # symlink to `object' that will be added to the tarball.
-  storeContents ? [ ]
-
+  storeContents ? [],
   # Extra commands to be executed before archiving files
-, extraCommands ? ""
-
+  extraCommands ? "",
   # extra inputs
-, extraInputs ? [ ]
-}:
-
-let
+  extraInputs ? [],
+}: let
   symlinks = map (x: x.symlink) storeContents;
   objects = map (x: x.object) storeContents;
 in
+  stdenv.mkDerivation {
+    name = "system-directory";
+    builder = ./make-system-directory.sh;
+    nativeBuildInputs = extraInputs;
 
-stdenv.mkDerivation {
-  name = "system-directory";
-  builder = ./make-system-directory.sh;
-  nativeBuildInputs = extraInputs;
+    inherit extraCommands;
 
-  inherit extraCommands;
+    # !!! should use XML.
+    sources = map (x: x.source) contents;
+    targets = map (x: x.target) contents;
 
-  # !!! should use XML.
-  sources = map (x: x.source) contents;
-  targets = map (x: x.target) contents;
+    # !!! should use XML.
+    inherit symlinks objects;
 
-  # !!! should use XML.
-  inherit symlinks objects;
-
-  closureInfo = closureInfo {
-    rootPaths = objects;
-  };
-}
+    closureInfo = closureInfo {
+      rootPaths = objects;
+    };
+  }
