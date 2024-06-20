f613ac694b2ed5baa5ef2402ebb5fa631592f947
Stefan Junker <mail@stefanjunker.de>
2024-06-20 16:42:03 +0200

    WIP: garage

--- modules/flake-parts/nixosModules.holo-users.nix
+++ modules/flake-parts/nixosModules.holo-users.nix
@@ -2,15 +2,18 @@
   inputs,
   lib,
   ...
-}: {
+}: let
+  mkAuthorizedKeys = {...}: {
+    keyFiles =
+      lib.attrValues
+      (lib.filterAttrs (name: _: lib.hasPrefix "keys_" name) inputs);
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHujII5RAwfEXNBYxKhWv2Wx/oHeHUTc8CACZ3M5W3p neonphog@gmail.com"
+    ];
+  };
+in {
   flake.nixosModules.holo-users = {
-    users.users.root.openssh.authorizedKeys = {
-      keyFiles =
-        lib.attrValues
-        (lib.filterAttrs (name: _: lib.hasPrefix "keys_" name) inputs);
-      keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICHujII5RAwfEXNBYxKhWv2Wx/oHeHUTc8CACZ3M5W3p neonphog@gmail.com"
-      ];
-    };
+    users.users.root.openssh.authorizedKeys = mkAuthorizedKeys {};
+    users.users.dev.openssh.authorizedKeys = mkAuthorizedKeys {};
   };
 }
