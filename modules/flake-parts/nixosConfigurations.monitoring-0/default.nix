422de16e54bcdf3290721f1f739e3e59abf40775
Stefan Junker <mail@stefanjunker.de>
2024-09-19 13:53:19 +0200

    feat: init monitoring-0 and add dns-entry

--- modules/flake-parts/nixosConfigurations.monitoring-0/default.nix
+++ modules/flake-parts/nixosConfigurations.monitoring-0/default.nix
@@ -4,7 +4,7 @@
   inputs,
   ...
 }: {
-  flake.nixosConfigurations.turn-2 = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.monitoring-0 = inputs.nixpkgs.lib.nixosSystem {
     modules = [./configuration.nix];
     system = "x86_64-linux";
     specialArgs = self.specialArgs;
