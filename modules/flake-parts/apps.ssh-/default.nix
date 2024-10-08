a175ee49a7aab66171ca2b6147ef3cd6ffc40824
Stefan Junker <mail@stefanjunker.de>
2024-10-08 16:09:22 +0200

    feat(apps-ssh): add ssh-<nix-build-distributor>-ping-builders command

--- modules/flake-parts/apps.ssh-/default.nix
+++ modules/flake-parts/apps.ssh-/default.nix
@@ -31,9 +31,46 @@
       config.apps =
         let
           configurations = (self.darwinConfigurations // self.nixosConfigurations);
+
           individual = lib.mapAttrs' mkSshApp configurations;
+
+          distributers = {
+            inherit (self.nixosConfigurations) linux-builder-01 buildbot-nix-0;
+          };
+          mkBuilderPingCommand =
+            distributorConfig:
+            builtins.concatStringsSep ''&& '' (
+              builtins.map (buildMachineAttrs: ''
+                (timeout 10s ssh -No StrictHostKeyChecking=accept-new ${buildMachineAttrs.sshUser}@${buildMachineAttrs.hostName} || true) && nix store info --store 'ssh-ng://${buildMachineAttrs.sshUser}@${buildMachineAttrs.hostName}'
+              '') distributorConfig.config.nix.buildMachines
+            );
+
+          /*
+            adds one command for every host that has the nix-build-distributor module imported.
+            this command will accept the host keys of all the configured buildMachines and check whether nix can successfully establish a remote connection.
+            note that execution takes 10 seconds per buildMachine, because the the key acceptance command hangs indefinitely in the success case.
+
+            run for example: `nix run .\#ssh-buildbot-nix-0-ping-builders
+          */
+          distributerNixPingBuilders = lib.mapAttrs' (
+            attrName: config:
+            lib.nameValuePair "${prefix}${attrName}-ping-builders" {
+              type = "app";
+              program = builtins.toString (
+                pkgs.writeShellScript "${prefix}${attrName}-ping-builders" ''
+                  exec ${
+                    mkSsh {
+                      inherit attrName;
+                      inherit (config.config) hostName deployUser;
+                    }
+                  } bash -c "${lib.strings.escapeShellArg (mkBuilderPingCommand config)}"
+                ''
+              );
+            }
+          ) distributers;
         in
         individual
+        // distributerNixPingBuilders
         // {
           "${prefix}all" = {
             type = "app";
